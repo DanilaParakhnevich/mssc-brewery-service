98564c26280856e0676795b35e4bc1b28c0aa959
dparakhnevich <DParakhnevich@iba.by>
2026-01-28 11:52:42 +0300

    Initial commit

---
+++ src/test/java/by/parakhnevich/mssc_brewery_service/MsscBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.parakhnevich.mssc_brewery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
