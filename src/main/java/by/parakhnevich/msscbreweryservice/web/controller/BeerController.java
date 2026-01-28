98ac914078d0ac7f8200ee3736a04a9ef7fca9cc
dparakhnevich <DParakhnevich@iba.by>
2026-01-28 12:31:17 +0300

    Created BeerController

---
+++ src/main/java/by/parakhnevich/msscbreweryservice/web/controller/BeerController.java
@@ -0,0 +1,29 @@
+package by.parakhnevich.msscbreweryservice.web.controller;
+
+import by.parakhnevich.msscbreweryservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
